1eee64a0a59893249fa7ce182af0b45885231ad9
fedalal <fedalal@gmail.com>
2026-06-27 12:57:41 +0300

    add function escapeHtml

--- app/static/app.js
+++ app/static/app.js
@@ -479,6 +479,15 @@ function relayNumOptions(selected){
   return opts.join("");
 }
 
+function escapeHtml(value){
+  return String(value ?? "")
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
+}
+
 function renderCfg(){
   if(!cfgState) return;
 
