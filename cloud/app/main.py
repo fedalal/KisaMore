4dd404daab0b77bea79d0e5214d8782c105a35ff
fedalal <fedalal@gmail.com>
2026-09-10 13:29:26 +0300

    Register Telegram admin management API

--- cloud/app/main.py
+++ cloud/app/main.py
@@ -24,6 +24,7 @@ from .rack_photo_api import router as rack_photo_router
 from .rack_photo_bootstrap import backfill_rack_photo_derivatives
 from .rental_admin_api import router as rental_admin_router
 from .seed_inventory_admin_api import router as seed_inventory_admin_router
+from .telegram_admin_api import router as telegram_admin_router
 from .timelapse_api import router as timelapse_router
 from .watering_admin_api import router as watering_admin_router
 
@@ -94,3 +95,4 @@ app.include_router(admin_camera_router)
 app.include_router(rental_admin_router)
 app.include_router(watering_admin_router)
 app.include_router(seed_inventory_admin_router)
+app.include_router(telegram_admin_router)
