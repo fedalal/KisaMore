93ec089877109be01ca8509517156827559998f3
fedalal <fedalal@gmail.com>
2026-09-12 21:56:41 +0300

    Install compact rental plant picker

--- cloud/app/telegram/worker_daily_photos.py
+++ cloud/app/telegram/worker_daily_photos.py
@@ -4,12 +4,14 @@ import asyncio
 
 from . import worker_admin as existing
 from .daily_photo_notifications import install
+from .rental_picker_ui import install as install_rental_picker_ui
 from .rental_progress_notifier import install as install_rental_progress
 
 
 core = existing.core
 install(core)
 install_rental_progress(core)
+install_rental_picker_ui()
 
 
 if __name__ == "__main__":
