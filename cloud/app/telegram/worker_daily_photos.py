b5c7c5f9373169a76cb140dccc99825b0ebfe1fe
fedalal <fedalal@gmail.com>
2026-09-13 11:19:52 +0300

    Enable Telegram gift feedback polish

--- cloud/app/telegram/worker_daily_photos.py
+++ cloud/app/telegram/worker_daily_photos.py
@@ -5,6 +5,7 @@ import asyncio
 from . import activity_notifier
 from . import worker_admin as existing
 from .daily_photo_notifications import install
+from .gift_feedback import install as install_gift_feedback
 from .plant_card_ui import install as install_plant_card_ui
 from .reaction_refresh import install as install_reaction_refresh
 from .rental_picker_ui import install as install_rental_picker_ui
@@ -18,6 +19,7 @@ install_rental_progress(core)
 install_rental_picker_ui()
 install_plant_card_ui(core)
 install_reaction_refresh(core)
+install_gift_feedback(core)
 install_watering_facts(activity_notifier)
 
 
