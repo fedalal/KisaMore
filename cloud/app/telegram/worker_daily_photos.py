738ce463bee87a657b639a790039a0b5e485632f
fedalal <fedalal@gmail.com>
2026-09-13 10:41:37 +0300

    Install Telegram plant card stats UI

--- cloud/app/telegram/worker_daily_photos.py
+++ cloud/app/telegram/worker_daily_photos.py
@@ -4,6 +4,7 @@ import asyncio
 
 from . import worker_admin as existing
 from .daily_photo_notifications import install
+from .plant_card_ui import install as install_plant_card_ui
 from .reaction_refresh import install as install_reaction_refresh
 from .rental_picker_ui import install as install_rental_picker_ui
 from .rental_progress_notifier import install as install_rental_progress
@@ -13,6 +14,7 @@ core = existing.core
 install(core)
 install_rental_progress(core)
 install_rental_picker_ui()
+install_plant_card_ui(core)
 install_reaction_refresh(core)
 
 
